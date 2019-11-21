9cf43e52c36570e35ef7d17e0a0598b36b69aa0a
Keerthan <keerthan.pai@iiitb.org>
2019-11-21 13:27:40 +0530

    Path variable in URI

---
+++ restful-web-services/readme.md
@@ -0,0 +1,18 @@
+## What You Will Need to run this project?
+
+We will help you install 
+- Java 8
+- IntelliJ
+- Maven
+- Embedded Tomcat
+- Postman REST Services Client
+
+## Useful Links
+
+- POSTMAN - http://www.getpostman.com
+
+### Links from examples implemented
+- Basic Resources
+  - http://localhost:8080/hello-world
+  - http://localhost:8080/hello-world-bean
+  - http://localhost:8080/hello-world/path-variable/TestUser
--- restful-web-services/src/main/java/com/sandbox/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
+++ restful-web-services/src/main/java/com/sandbox/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
@@ -1,6 +1,7 @@
 package com.sandbox.rest.webservices.restfulwebservices.helloworld;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -20,5 +21,11 @@ public class HelloWorldController {
     @GetMapping(path = "/hello-world-bean")
     public HelloWorldBean helloWorldBean() {
         return new HelloWorldBean("Hello World!");
+    }
+
+    //hello-world-bean/path-variable/someUser
+    @GetMapping(path = "/hello-world-bean/path-variable/{name}")
+    public HelloWorldBean helloWorldBean(@PathVariable String name) {
+        return new HelloWorldBean( String.format("Hello world, %s!", name));
     }
 }
